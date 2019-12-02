31f34b6dc8870f6c290aa5ee7b8e654a099eb44c
nahu995 <nahu995@gmail.com>
2019-12-02 12:15:35 -0300

    Updates

---
+++ precache-manifest.9c7580d08c5ca5a91753f7b37e4f2e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d5bcf490caee12750803c6e5ee0a0d",
+    "url": "/glamit/index.html"
+  },
+  {
+    "revision": "426f0c847545d3b28dc6",
+    "url": "/glamit/static/css/main.994a3eca.chunk.css"
+  },
+  {
+    "revision": "8f6c343642f3ca731ace",
+    "url": "/glamit/static/js/2.43734a16.chunk.js"
+  },
+  {
+    "revision": "426f0c847545d3b28dc6",
+    "url": "/glamit/static/js/main.f2e08573.chunk.js"
+  },
+  {
+    "revision": "79e72a80d72857014a77",
+    "url": "/glamit/static/js/runtime-main.7a5278a6.js"
+  },
+  {
+    "revision": "f2e8a62e351e9ad9c1391db69b938b25",
+    "url": "/glamit/static/media/imagen.f2e8a62e.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/glamit/precache-manifest.9c7580d08c5ca5a91753f7b37e4f2e36.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/glamit/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
